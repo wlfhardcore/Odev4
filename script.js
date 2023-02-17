c20032f48d97405a9509b3d75cdadea85c8a70e6
wlfhardcore <f.e.uras68l@gmail.com>
2023-02-17 22:45:56 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var x= 11;
+var y= 24;
+function toplama(){
+    alert(x+y);
+}
+function cikarma(){
+    alert(x-y);
+}
+function carpma(){
+    alert(x*y);
+}
+function bolme(){
+    alert(x/y);
+}
+function mod(){
+    alert(x%y);
+}
